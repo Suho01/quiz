f23220fbc259fc8373f125aa18abb53d5a564742
이지연 <peaceprimavera@gmail.com>
2023-09-11 10:14:54 +0900

    0911

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
     const [quizList, setQuizList] = useState(QuizList);
     const quiz = [...QuizList];
     const [selected, setSelected] = useState(3);
+    const [quizCnt, setQuizCnt] = useState(QuizList.length);
+    const [typeTxt, setTypetxt] = useState("전체");
 
     const ChangeEvent = (data) => {
         const classValue = data.target.className;
@@ -26,25 +28,26 @@ function App() {
             case classValue.includes("random") :
                 (dataValue === "1" ?
                 setQuizList([...QuizList].sort(() => {
-                    return Math.random() - 0.5
-                }))
-                : setQuizList([...QuizList]));
+                    return Math.random() - 0.5}).slice(0, selected)
+                )
+                : setQuizList([...QuizList]).slice(0, selected));
                 // 0~1 사이의 값을 반환 : 0.5의 평균값을 얻게 되어 -0.5~0.5 값으로 랜덤 출력
             break;
 
             case classValue.includes("cnt") :
-                console.log("갯수 바뀜");
+                setSelected(dataValue);
             break;
 
             case classValue.includes("type") :
                 (dataValue === "전체"
                     ?
-                    setQuizList([...QuizList])
+                    setQuizList([...QuizList].slice(0, selected))
                     :
                     setQuizList([...QuizList].filter((e) => {
                         return e.type === dataValue;
-                    }))
+                    }).slice(0, selected))
                 )
+                setTypetxt(dataValue);
             break;
 
             default : // 맨마지막에만 넣을수있음
@@ -53,12 +56,17 @@ function App() {
     }
 
     useEffect(() => { // 일정 시간이 지난 뒤에 로딩이 된 후 실행되는 함수, 재 렌더링 될때마다 끝나고 실행됨(새로고침 등)
-        console.log(quizList);
-    }, [quizList]); // 로딩되고난후 한번만 실행시키려면, [](배열) 추가
+        setQuizList([...QuizList].slice(0, selected));
+        setQuizCnt([...QuizList].filter((e) => {
+            return typeTxt === "전체" ? true : e.type === typeTxt;
+        }).length);
+    }, [typeTxt, selected]); // 로딩되고난후 한번만 실행시키려면, [](배열) 추가
+
+    console.log(quizList);
 
     return (
         <Routes>
-            <Route path="/" element={<Main ChangeEvent={ChangeEvent} userName={userName} quiz={quiz} selected={selected} quizList={quizList} />} />
+            <Route path="/" element={<Main ChangeEvent={ChangeEvent} userName={userName} quiz={quiz} selected={selected} quizList={quizList} quizCnt={quizCnt} />} />
             <Route path="/quiz" element={<Detail quizList={quizList} userName={userName} />} />
         </Routes>
     );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-function Main({userName, ChangeEvent, quizList, quiz, selected}) {
+function Main({userName, ChangeEvent, quizList, quiz, selected, quizCnt}) {
 
     let navigate = useNavigate();
 
@@ -48,7 +48,7 @@ function Main({userName, ChangeEvent, quizList, quiz, selected}) {
             <button className='btn-primary text-sm sm:text-base bg-green-800 hover:bg-green-700 focus:ring-green-400 basis-5/12 my-5'>갯수설정</button>
             <select className='cnt border rounded basis-6/12 text-center py-1.5' onChange={ChangeEvent} defaultValue={selected}>
                 {
-                    Array(quizList.length).fill().map((e, i) => {
+                    Array(quizCnt).fill().map((e, i) => {
                         return <option value={i + 1} key={i}>{i + 1}문제</option>
                     })
                 }
